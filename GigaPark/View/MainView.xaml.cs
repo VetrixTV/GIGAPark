4bb219a94274cf1ba27cbef6684e2364cbc4ab75
WinzuDarkpaw <niconowak3@gmail.com>
2022-05-24 19:03:21 +0200

    Projektdatei kann jetzt auch geöffnet werden, Digga

--- GigaPark/View/MainView.xaml.cs
+++ GigaPark/View/MainView.xaml.cs
@@ -1,33 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GigaPark.View
 {
     /// <summary>
-    /// Interaktionslogik für MainView.xaml
+    ///     Interaktionslogik für MainView.xaml
     /// </summary>
     public partial class MainView : Window
     {
         public MainView()
         {
             InitializeComponent();
-            Test();
+            ResizeMode = ResizeMode.NoResize;
         }
 
-        private void Test()
+        /// <summary>
+        ///     Behandelt die Interaktion mit dem Button "EntranceButton".
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EntranceButton_Click(object sender, RoutedEventArgs e)
         {
-            TestTextBox.Text = "Hallo Welt!";
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Behandelt die Interaktion mit dem Button "ExitButton".
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Behandelt die Interaktion mit dem Button "ExitButtonLongtermCustomer".
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButtonLongtermCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
