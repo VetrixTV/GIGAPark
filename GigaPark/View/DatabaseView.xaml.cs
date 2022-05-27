a8a63b0ca180ad652248e127de2d14eac00d8a8c
Nico Nowak (alias Winzu) <50320269+WinzuDarkpaw@users.noreply.github.com>
2022-05-27 13:52:58 +0200

    View für die Datenbank und Anpassungen

---
+++ GigaPark/View/DatabaseView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using GigaPark.Database.Helpers;
+
+namespace GigaPark.View
+{
+    /// <summary>
+    ///     Interaction logic for DatabaseView.xaml
+    /// </summary>
+    public partial class DatabaseView : Window
+    {
+        private readonly DataContext _context;
+
+        public DatabaseView(DataContext context)
+        {
+            InitializeComponent();
+
+            _context = context;
+
+            ParkplatzGrid.ItemsSource = _context.Parkplatz.Local.ToObservableCollection();
+            ParkscheinGrid.ItemsSource = _context.Parkschein.Local.ToObservableCollection();
+        }
+    }
+}
--- GigaPark/View/MainView.xaml.cs
+++ GigaPark/View/MainView.xaml.cs
@@ -1,17 +1,51 @@
-﻿using System;
+﻿/*
+ *  MainView.xaml.cs
+ *  Autor: Nico Nowak, Erik Ansmann, Wilhelm Adam
+ */
+
+using System;
 using System.Windows;
+using GigaPark.Database.Entities;
+using GigaPark.Database.Helpers;
+using Microsoft.EntityFrameworkCore;
 
 namespace GigaPark.View
 {
     /// <summary>
     ///     Interaktionslogik für MainView.xaml
     /// </summary>
-    public partial class MainView : Window
+    public partial class MainView
     {
+        /// <summary>
+        ///     Datenbankkontext.
+        /// </summary>
+        private readonly DataContext _context = new();
+
+        /// <summary>
+        ///     Initialisiert eine neue Instanz der <see cref="MainView" />-Klasse.
+        /// </summary>
         public MainView()
         {
             InitializeComponent();
+
+            // Das Fenster soll sich nicht vergrößern/verkleinern lassen.
             ResizeMode = ResizeMode.NoResize;
+
+            // Services initialisieren.
+            InitializeServices();
+        }
+
+        /// <summary>
+        ///     Initialisiert die im Programm genutzten Services und Kontexte.
+        /// </summary>
+        private void InitializeServices()
+        {
+            // Stellt sicher, dass die Datenbank existiert.
+            _context.Database.EnsureCreated();
+
+            // Lädt die Datentabellen aus dem Kontext und stellt diese zur Bearbeitung zur Verfügung.
+            _context.Parkplatz.Load();
+            _context.Parkschein.Load();
         }
 
         /// <summary>
@@ -42,6 +76,17 @@ namespace GigaPark.View
         private void ExitButtonLongtermCustomer_Click(object sender, RoutedEventArgs e)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Behandelt die Interaktion mit dem Button "ShowDatabaseButton"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShowDatabaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            DatabaseView dbView = new(_context);
+            dbView.Show();
         }
     }
 }
